ccfb6e70dfe5b7920e1cbb07a9f1be92b25845c9
Makism Khrabrykh <65070875+Neonexer@users.noreply.github.com>
2025-05-04 18:35:47 +0700

    header and sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,9 +9,12 @@ import OnboardingPage from "./pages/OnboardingPage.jsx";
 import { Toaster } from "react-hot-toast";
 import PageLoader from './components/PageLoader.jsx';
 import useAuthUser from './hooks/useAuthUser.js';
+import Layout from './components/Layout.jsx';
+import { useThemeStore } from './store/useThemeStore.js';
 
 const App = () => {
   const { isLoading, authUser } = useAuthUser();
+  const { theme } = useThemeStore();
 
   const isAuthenticated = Boolean(authUser);
   const isOnboarded = authUser?.isOnboarded;
@@ -19,10 +22,12 @@ const App = () => {
   if (isLoading) return <PageLoader/>;
 
   return (
-    <div className='h-screen' data-theme="night">
+    <div className='h-screen' data-theme={theme}>
       <Routes>
         <Route path='/' element={ isAuthenticated && isOnboarded ? (
-          <HomePage />
+          <Layout showSidebar={true}>
+            <HomePage />
+          </Layout>
         ) : (
           <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />  
         ) } />
